f46bbe988fe0a45d8f2326e0f505d6fbdf33ba23
ByeongJuP <pbju92@gmail.com>
2022-08-22 10:54:31 +0900

    fix: not used code delete

--- src/components/SignUp/index.tsx
+++ src/components/SignUp/index.tsx
@@ -15,7 +15,6 @@ const SingUp: React.FC<PropTypes> = () => {
     handleSubmit,
     setError,
     setFocus,
-    watch,
     formState: { errors }
   } = useForm<SingUpType>();
   const onSubmit: SubmitHandler<SingUpType> = (data) => {
@@ -25,17 +24,6 @@ const SingUp: React.FC<PropTypes> = () => {
       setFocus('secondPassword');
     }
   };
-  console.log(errors);
-  // useEffect(() => {
-  //   const firstError = (Object.keys(errors) as Array<keyof typeof errors>).reduce<keyof typeof errors | null>((field, a) => {
-  //     const fieldKey = field as keyof typeof errors;
-  //     return !!errors[fieldKey] ? fieldKey : a;
-  //   }, null);
-
-  //   if (firstError) {
-  //     setFocus(firstError);
-  //   }
-  // }, [errors, setFocus]);
 
   return (
     <div className={`${baseClassName}`}>
