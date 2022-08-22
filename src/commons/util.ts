15686bbf3baa8e51a54c630e2ff0ec4c6317200c
ByeongJuP <pbju92@gmail.com>
2022-08-22 13:23:01 +0900

    add: header login state

--- src/commons/util.ts
+++ src/commons/util.ts
@@ -1,1 +1,13 @@
 export const regEmail = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+export const isLogin = () => {
+  return !!sessionStorage.getItem('user');
+};
+export const userInfo = () => {
+  const sessionUser = sessionStorage.getItem('user');
+  return sessionUser ? JSON.parse(sessionUser) : null;
+};
+
+export const deleteUserSession = () => {
+  sessionStorage.removeItem('token');
+  sessionStorage.removeItem('user');
+};
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { withRouter } from 'react-router';
 import { Link } from 'react-router-dom';
+import { deleteUserSession, isLogin, userInfo } from '../../commons/util';
 
 interface PrpoTypes {}
 
 const Header: React.FC<PrpoTypes> = ({}) => {
+  const loginState = isLogin();
+  const getUserInfo = userInfo();
   return (
     <div className="flex pa1 justify-between nowrap orange">
       <div className="flex flex-fixed black">
-        <div className="fw7 mr1">Hacker News</div>
         <Link to="/" className="ml1 no-underline black">
           LIST
         </Link>
@@ -17,14 +19,33 @@ const Header: React.FC<PrpoTypes> = ({}) => {
           NEW
         </Link>
         <div className="ml1">|</div>
-        <Link to="/signUp" className="ml1 no-underline black">
-          SignUp
-        </Link>
-        <div className="ml1">|</div>
-        <Link to="/signIn" className="ml1 no-underline black">
-          SignIn
-        </Link>
+        {loginState ? (
+          <span
+            className="ml1 no-underline black"
+            style={{ cursor: 'pointer' }}
+            onClick={() => {
+              deleteUserSession();
+              window.location.href = '/';
+            }}>
+            SignOut
+          </span>
+        ) : (
+          <>
+            <Link to="/signUp" className="ml1 no-underline black">
+              SignUp
+            </Link>
+            <div className="ml1">|</div>
+            <Link to="/signIn" className="ml1 no-underline black">
+              SignIn
+            </Link>
+          </>
+        )}
       </div>
+      {getUserInfo && (
+        <div className="black">
+          <span>👤{getUserInfo && getUserInfo.name}님.</span>
+        </div>
+      )}
     </div>
   );
 };
